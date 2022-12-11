57234fadda051e2cb0994b65203823554befb0b6
BarsChronicle <rzhang88@binghamton.edu>
2022-12-11 11:15:24 -0500

    fixed minor syntax errors

--- Power_Rec_Feature/rec_audio.py
+++ Power_Rec_Feature/rec_audio.py
@@ -1,11 +1,12 @@
 import sounddevice as sd
-
+import soundfile as sf
 
 duration = 60
 fs = 44100
 print(sd.query_devices())
 
+sd.default.device = 2
 myrecording = sd.rec(int(duration*fs), samplerate=fs,channels=1)
 
 sd.wait()
-sf.write("test_rec.wav",date=myrecording,samplerate=1000)
+sf.write("test_rec.wav",data=myrecording,samplerate=1000)
