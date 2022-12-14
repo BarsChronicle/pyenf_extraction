47ad5b927119dea38af83b6a39be8946afdf6de3
BarsChronicle <rzhang88@binghamton.edu>
2022-12-14 16:37:36 -0500

    Added downsampling feature when writing audio file

--- Power_Rec_Feature/power_rec_odd_hr.py
+++ Power_Rec_Feature/power_rec_odd_hr.py
@@ -7,14 +7,18 @@ import sys
 import os
 
 folder_audio = "Power_Recordings/"
+downsample_factor = 5
 
 def power_rec(fs, duration, path): #buffer audio
     print(f"Enter recording: {duration} seconds")
-    sd.default.device = 2
+    sd.default.device = 11
     buf_recording = sd.rec(int(duration * fs), samplerate=fs, channels=1) # buffer for the duration
-
     sd.wait()
-    sf.write(folder_audio+path+".wav", data=buf_recording, samplerate=1000)
+
+    buf_recording = buf_recording[::downsample_factor] # downsample by Fs by a factor
+    sr = int(fs/downsample_factor)
+
+    sf.write(folder_audio+path+".wav", data=buf_recording, samplerate=sr)
     print("Exit recording")
 
 def restart():
